6da7d35cef5a0083063717a1b6e6b0cb6cda5c02
thoppe <travis.hoppe@gmail.com>
2017-06-18 14:27:25 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -2,18 +2,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "TorSpiders",
+    name = "torspiders",
     description="Spiders a website using the darknet via Tor.",
     author="Travis Hoppe",
-    author_email="travis.hoppe+github.tor_spiders@gmail.com",
+    author_email="travis.hoppe+tor_spiders@gmail.com",
     url="https://github.com/thoppe/tor_spiders",
-    version = "1.0",
+    version = "1.1",
     packages = find_packages(),
-    install_requires = [
-        'certifi>=2015.4.28',
-        'chardet>=2.3.0',
-        'requesocks>=0.10.8',
-        'stem>=1.4.0',
-        'wheel>=0.24.0',
-    ]
 )
